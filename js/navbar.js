6aa6e9dcf327318c38a7324c9e54a1fa51b35d3e
Jaroslav Nemec <j.nemecc9@gmail.com>
2023-10-04 12:46:24 +0200

    improve navbar

--- js/navbar.js
+++ js/navbar.js
@@ -2,5 +2,13 @@
 
 function ToggleMenu(){
     let navbar = document.getElementById("navbar");
+    let navbar_toggler = document.getElementById("navbar-toggler");
+    
     navbar.classList.toggle('collapsed');
+
+    if(navbar.classList.contains('collapsed')){
+        navbar_toggler.style.display = "none";
+    }else{
+        navbar_toggler.style.display = "block";
+    }
 }
