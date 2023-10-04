94e56a242681785559d8492eb4a69413c347272a
Jaroslav Nemec <j.nemecc9@gmail.com>
2023-10-04 11:15:37 +0200

    fix navbar collapsing and animation

--- js/navbar.js
+++ js/navbar.js
@@ -1,14 +1,6 @@
 
 
 function ToggleMenu(){
-    let navbar = document.getElementById("navbar")
-
-    if(navbar.classList.contains("navbar-hidden")){
-        navbar.classList.remove("navbar-hidden")
-        navbar.classList.add("navbar-visible")
-    }
-    else{
-        navbar.classList.remove("navbar-visible")
-        navbar.classList.add("navbar-hidden")
-    }
+    let navbar = document.getElementById("navbar");
+    navbar.classList.toggle('collapsed');
 }
