1c226af496d07cec0012a973393b33164c876d6f
Jaroslav Nemec <j.nemecc9@gmail.com>
2024-01-23 22:38:58 +0100

    edit navbar so it closes after clicking the link

--- js/navbar.js
+++ js/navbar.js
@@ -1,14 +1,16 @@
-
-
 function ToggleMenu(){
+    let screen_width = window.innerWidth;
     let navbar = document.getElementById("navbar");
     let navbar_toggler = document.getElementById("navbar-toggler");
     
-    navbar.classList.toggle('collapsed');
+    if(screen_width < 1420){
 
-    if(navbar.classList.contains('collapsed')){
-        navbar_toggler.style.display = "none";
-    }else{
-        navbar_toggler.style.display = "block";
+        navbar.classList.toggle('collapsed');
+
+        if(navbar.classList.contains('collapsed')){
+            navbar_toggler.style.display = "none";
+        }else{
+            navbar_toggler.style.display = "block";
+        }
     }
 }
