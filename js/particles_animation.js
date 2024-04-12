7d9488e4422fafaa921a84cd62a6e1c16d4f120f
Jaroslav Nemec <j.nemecc9@gmail.com>
2024-04-12 08:43:19 +0200

    finish particles animation

--- js/particles_animation.js
+++ js/particles_animation.js
@@ -1,31 +1,16 @@
 const canvas = document.getElementById('animation-canvas');
-
-// canvas.width = window.innerWidth;
-// canvas.height = window.innerHeight;
-
 const ctx = canvas.getContext('2d');
 const direction = [-1, 1];
 const color = '#BF4526';
-const numberOfParticles = 8;
-// console.log(numberOfParticles);
-const maxRadius = window.innerHeight / 100;
-const minRadius = window.innerHeight / 1000;
+const numberOfParticles = 50;
+const maxRadius = canvas.width / 180;
+const minRadius = canvas.width / 300;
 const maxVelocity = 1;
-// console.log(maxRadius);
 
 const particles = [];
-let collision = false;
 
-
-
-
-// console.log(Math.random()*color.length)
-// console.log(color)
-
-
-
-class Particle{
-    constructor(x, y, radius, particle_color){
+class Particle {
+    constructor(x, y, radius, particle_color) {
         this.x = x;
         this.y = y;
         this.points = [];
@@ -37,18 +22,18 @@ class Particle{
         this.velocityY = Math.ceil(Math.random() * maxVelocity);
     }
 
-    move(){
+    move() {
         this.x += this.velocityX * this.directionX;
         this.y += this.velocityY * this.directionY;
-        if(this.x + this.radius > canvas.width || this.x - this.radius/3 < 0){
+        if (this.x + this.radius > canvas.width || this.x - this.radius / 3 < 0) {
             this.directionX *= -1;
         }
-        if(this.y + this.radius > canvas.height || this.y - this.radius/3 < 0){
+        if (this.y + this.radius > canvas.height || this.y - this.radius / 3 < 0) {
             this.directionY *= -1;
         }
     }
 
-    draw(){
+    draw() {
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
         ctx.fillStyle = this.color;
@@ -56,59 +41,59 @@ class Particle{
         ctx.closePath();
     }
 
-    getPoints(){
+    getPoints() {
         this.points = [];
-        for (let i = 0; i < this.radius*Math.PI/2; i+=0.2){
+        for (let i = 0; i < this.radius * Math.PI / 2; i += 0.1) {
             const x = Math.ceil(this.x + this.radius * Math.cos(i));
             const y = Math.ceil(this.y + this.radius * Math.sin(i));
 
             this.points.push([x, y]);
-        
-            // ctx.beginPath();
-            // ctx.rect(x, y, 1, 1);
-            // ctx.fillStyle ='white';
-            // ctx.fill();
-            // ctx.closePath();
-            
+        }
+    }
+
+    checkCollision(particle) {
+        const dx = particle.x - this.x;
+        const dy = particle.y - this.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.radius + particle.radius) {
+            const angle = Math.atan2(dy, dx);
+            const sine = Math.sin(angle);
+            const cosine = Math.cos(angle);
+
+            const vx1 = this.velocityX * cosine + this.velocityY * sine;
+            const vy1 = this.velocityY * cosine - this.velocityX * sine;
+            const vx2 = particle.velocityX * cosine + particle.velocityY * sine;
+            const vy2 = particle.velocityY * cosine - particle.velocityX * sine;
+
+            this.velocityX = vx2;
+            this.velocityY = vy2;
+            particle.velocityX = vx1;
+            particle.velocityY = vy1;
         }
     }
 }
 
-function init(){
-    for(let i = 0; i < numberOfParticles; i++){
-        const x = ((canvas.width/numberOfParticles)*i)+20;
+function init() {
+    for (let i = 0; i < numberOfParticles; i++) {
+        const x = ((canvas.width / numberOfParticles) * i) + 20;
         const y = 100;
         particles.push(new Particle(x, y, color));
     }
 }
 
-function animate(){
+function animate() {
     requestAnimationFrame(animate);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     particles.forEach(particle => {
         particle.getPoints();
-        collision = false;
-
         particles.forEach(particle2 => {
-            if (particle != particle2){
-                for (let i = 0; i < particle.points.length; i++){
-                    for (let j = 0; j < particle2.points.length; j++){
-                        if (particle.points[i][0] == particle2.points[j][0] && particle.points[i][1] == particle2.points[j][1]){
-                            // console.log('collision');
-                            collision = true;
-                        }
-                    }
-                }
+            if (particle !== particle2) {
+                particle.checkCollision(particle2);
             }
         });
-        if (collision){
-            particle.directionX *= -1;
-            particle.directionY *= -1;
-        }
-
         particle.draw();
-        particle.move(); 
-        allPoints = {};
+        particle.move();
     });
 }
 
